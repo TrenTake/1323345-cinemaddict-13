37afecc3534d0f0089906772ea7507a31a1bf309
TrenTake <stephanie_bobyr10@mail.ru>
2020-11-25 20:05:34 +0200

    Добавление компонентов

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import {createProfileTemplate} from 'view/profile';
-import {createMainNavigationTemplate} from 'view/navigation';
-import {createSortTemplate} from 'view/sort';
-import {createFilmDetailsTemplate} from 'view/details';
-import {createShowMoreButtonTemplate} from 'view/show-button';
-import {createFilmCardTemplate} from 'view/card';
+
+import {createProfileTemplate} from './view/profile';
+import {createMainNavigationTemplate} from './view/navigation';
+import {createSortTemplate} from './view/sort';
+import {createFilmDetailsTemplate} from './view/details';
+import {createShowMoreButtonTemplate} from './view/show-button';
+import {createFilmCardTemplate} from './view/card';
+import {createFilmContainerTemplate} from './view/container';
 
 
 const CARDS_COUNT = 5;
@@ -18,13 +20,14 @@ const bodyElement = document.querySelector(`body`);
 const headerElement = bodyElement.querySelector(`.header`);
 const mainElement = bodyElement.querySelector(`.main`);
 const filmContainerElement = bodyElement.querySelector(`.films-list`);
-const filmsListElement = filmContainerElement.querySelector(`.films-list__container`);
 const filmsExtraListElements = bodyElement.querySelectorAll(`.films-list--extra`);
 
 render(headerElement, createProfileTemplate(), `afterbegin`);
 render(mainElement, createMainNavigationTemplate(), `afterbegin`);
 render(mainElement, createSortTemplate(), `afterbegin`);
 render(filmContainerElement, createShowMoreButtonTemplate(), `afterbegin`);
+render(mainElement, createFilmContainerTemplate(), `afterbegin`);
+
 for (card of CARDS_COUNT) {
   render(filmsListElement, createFilmCardTemplate(), `afterbegin`);
 }
