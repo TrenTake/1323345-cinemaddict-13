1563edae8c9f8f1c90d6b7c6c1ab02ddcdf7454d
TrenTake <stephanie_bobyr10@mail.ru>
2020-11-29 17:12:10 +0200

    components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import {createProfileTemplate} from './view/profile';
 import {createMainNavigationTemplate} from './view/navigation';
 import {createSortTemplate} from './view/sort';
@@ -6,7 +5,7 @@ import {createFilmDetailsTemplate} from './view/details';
 import {createShowMoreButtonTemplate} from './view/show-button';
 import {createFilmCardTemplate} from './view/card';
 import {createFilmContainerTemplate} from './view/container';
-
+import {createStatisticsTemplate} from './view/statistics';
 
 const CARDS_COUNT = 5;
 const EXTRA_CARDS_COUNT = 2;
@@ -16,28 +15,29 @@ const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const bodyElement = document.querySelector(`body`);
-const headerElement = bodyElement.querySelector(`.header`);
-const mainElement = bodyElement.querySelector(`.main`);
-const filmContainerElement = bodyElement.querySelector(`.films-list`);
-const filmsExtraListElements = bodyElement.querySelectorAll(`.films-list--extra`);
+const headerElement = document.body.querySelector(`.header`);
+render(headerElement, createProfileTemplate(), `beforeend`);
+const mainElement = document.body.querySelector(`.main`);
+render(mainElement, createMainNavigationTemplate(), `beforeend`);
+render(mainElement, createSortTemplate(), `beforeend`);
+render(mainElement, createFilmContainerTemplate(), `beforeend`);
+const filmsExtraListElements = document.body.querySelectorAll(`.films-list--extra`);
 
-render(headerElement, createProfileTemplate(), `afterbegin`);
-render(mainElement, createMainNavigationTemplate(), `afterbegin`);
-render(mainElement, createSortTemplate(), `afterbegin`);
-render(filmContainerElement, createShowMoreButtonTemplate(), `afterbegin`);
-render(mainElement, createFilmContainerTemplate(), `afterbegin`);
-
-for (card of CARDS_COUNT) {
-  render(filmsListElement, createFilmCardTemplate(), `afterbegin`);
+const filmContainerElement = document.body.querySelector(`.films-list`);
+const cardWrapperElement = document.querySelector(`.films-list .films-list__container`);
+for (let i = 1; i <= CARDS_COUNT; i++) {
+  render(cardWrapperElement, createFilmCardTemplate(), `beforeend`);
 }
 
-for (card of EXTRA_CARDS_COUNT) {
-  render(filmsExtraListElements[0].querySelector(`.films-list__container`), createFilmCardTemplate(), `afterbegin`);
+for (let i = 1; i <= EXTRA_CARDS_COUNT; i++) {
+  render(filmsExtraListElements[0].querySelector(`.films-list__container`), createFilmCardTemplate(), `beforeend`);
 }
 
-for (card of EXTRA_CARDS_COUNT) {
-  render(filmsExtraListElements[1].querySelector(`.films-list__container`), createFilmCardTemplate(), `afterbegin`);
+for (let i = 1; i <= EXTRA_CARDS_COUNT; i++) {
+  render(filmsExtraListElements[1].querySelector(`.films-list__container`), createFilmCardTemplate(), `beforeend`);
 }
 
-render(bodyElement, createFilmDetailsTemplate(), `afterbegin`);
+render(filmContainerElement, createShowMoreButtonTemplate(), `beforeend`);
+const filmStatisticsElement = document.body.querySelector(`.footer__statistics`);
+render(filmStatisticsElement, createStatisticsTemplate(), `beforeend`);
+// render(document.body, createFilmDetailsTemplate(), `beforeend`);
