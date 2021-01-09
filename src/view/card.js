aa50cf8113d95d4246a07fea4ea82e3fb2b1f806
TrenTake <stephanie_bobyr10@mail.ru>
2021-01-09 15:48:43 +0200

    eslint

--- src/view/card.js
+++ src/view/card.js
@@ -9,7 +9,7 @@ export const createFilmCardTemplate = (movie) => {
             </p>
             <img src="./images/posters/${movie.poster}" alt="" class="film-card__poster">
             <p class="film-card__description">${movie.description}</p>
-            <a class="film-card__comments">${movie.comments.length + " comments"}</a>
+            <a class="film-card__comments">${movie.comments.length + ` comments`}</a>
             <div class="film-card__controls">
               <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
               <button class="film-card__controls-item button film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
